56c511ef2c5b5d2c6862dd8caa136d41ce9ea071
Gabriel Brotas <gabriel.brotas@wexinc.com>
2022-10-05 10:03:48 -0300

    new notification type

--- src/db.js
+++ src/db.js
@@ -1,5 +1,7 @@
 export const notification_types = {
     "LIKED_POST": "LIKED_POST",
+    "STARTED_FOLLOW": "STARTED_FOLLOW",
+    "MESSAGE_RECEIVED": "MESSAGE_RECEIVED",
 }
 
 export const db_notifications = {
@@ -14,6 +16,21 @@ export const db_notifications = {
                 receiver: "Gabriel",
                 sender: "Marvolo",
                 type: notification_types.LIKED_POST
+            },
+            {
+                receiver: "Gabriel",
+                sender: "Ronald Wesley",
+                type: notification_types.MESSAGE_RECEIVED
+            },
+            {
+                receiver: "Gabriel",
+                sender: "Hermione",
+                type: notification_types.MESSAGE_RECEIVED
+            },
+            {
+                receiver: "Gabriel",
+                sender: "Hagrid",
+                type: notification_types.STARTED_FOLLOW
             }
         ]
     }
